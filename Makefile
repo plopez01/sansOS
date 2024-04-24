0d5648437b827ffd79154f5a7065cd77a2e594d4
plopez01 <plopez.01.info@gmail.com>
2024-04-24 19:45:01 +0200

    Experiments with multiboot

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 
 all : sansos.iso
 qemu : sansos.iso
-	qemu-system-i386 -cdrom sansos.iso
+	qemu-system-i386 -cdrom sansos.iso -s
 usb : sansos.iso
 	sudo dd if=sansos.iso of=/dev/sda && sync
 clean:
@@ -12,10 +12,10 @@ boot.o : arch/i386/boot.S
 	nasm -f elf32 $< -o $@
 
 kernel.o : kernel/kernel.c
-	i686-elf-gcc -c $< -o $@ -ffreestanding -O2 -Wall -Wextra -std=gnu99
+	i686-elf-gcc -c $< -o $@ -ffreestanding -O0 -Wall -Wextra -std=gnu99
 
 sansos.bin : kernel.o boot.o linker.ld
-	i686-elf-gcc -T linker.ld -o $@ -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+	i686-elf-gcc -T linker.ld -o $@ -ffreestanding -O0 -nostdlib boot.o kernel.o -lgcc
 
 sansos.iso : sansos.bin grub.cfg
 	mkdir -p isodir/boot/grub
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,7 +1,52 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include "include/multiboot.h"
 
-void kernel_main(uint32_t* multiboot){
-	//*(multiboot + 88)
+bool isNumAligned(int num) {
+    return (num & 0b111) == 0;
+}
+
+int alignNum(int num){
+    if (isNumAligned(num)) return num;
+    else return num - (num & 0b111) + 8;
+}
+
+void kernel_main(multiboot_uint32_t* multiboot){
+	register int eax asm("eax");
+  // Indexed color
+	/*if (multiboot->framebuffer_type == 0){
+		unsigned char* palette = (unsigned char*)multiboot->framebuffer_palette_addr;
+		*palette = 0xFF;
+		*(palette+1) = 0x0;
+		*(palette+2) = 0x0;
+
+		*((uint64_t*) multiboot->framebuffer_addr) = 1;
+		*((uint64_t*) multiboot->framebuffer_addr + 100*multiboot->framebuffer_pitch + 100) = 0;
+	} else if (multiboot->framebuffer_type == 1) {
+
+	} else {
+		// Unsupported EGA text mode
+		// TODO: Print message saying so
+	  *((uint64_t*)0xB8000) = 'j'*16 + 0x0f;
+  }*/
+
+  multiboot_uint32_t total_size = *multiboot;
+  int size = 2;
+  multiboot_uint32_t* tag_selector = multiboot + size;
+  
+  for (int i = 0; i < 20; i++) {
+    // Tag header
+    struct multiboot_tag* tag_header = (struct multiboot_tag*)tag_selector;
+
+    *((int*)0) = tag_header->type;
+    *((int*)4) = tag_header->size; 
+    *((int*)8) = tag_selector - multiboot;
+    // Calculate next tag address
+    tag_selector += alignNum(tag_header->size)/sizeof(multiboot_uint32_t); 
+    eax = tag_header->type;
+    if  (tag_header->type == 8) break;
+  }
+
+  *((short*)753664) = 0x0f00 + 'j';
 }
